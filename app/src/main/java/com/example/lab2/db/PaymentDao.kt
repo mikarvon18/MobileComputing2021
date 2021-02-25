a89c7d4efd9dc96a699e0e9aeb71cd9c5744ac89
Mikarvon18 <karvonenmiksu@gmail.com>
2021-02-25 12:16:31 +0200

    final commit for HW2

--- app/src/main/java/com/example/lab2/db/PaymentDao.kt
+++ app/src/main/java/com/example/lab2/db/PaymentDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Transaction
+import java.util.*
 
 @Dao
 interface PaymentDao {
@@ -11,9 +12,17 @@ interface PaymentDao {
     @Insert
     fun insert(paymentInfo: PaymentInfo): Long
 
+
     @Query("DELETE FROM paymentInfo WHERE uid = :id")
     fun delete(id: Int)
 
     @Query("SELECT * FROM paymentInfo")
     fun getPaymentInfos(): List<PaymentInfo>
+
+    @Query("SELECT * FROM paymentInfo WHERE uid = :id")
+    fun getPaymentInfo(id: Int): List<PaymentInfo>
+
+    @Query("UPDATE paymentInfo SET title=:title, date=:date, locationX=:locationX, locationY=:locationY WHERE uid = :id")
+    fun editTable(title: String, date: String, locationX: String, locationY: String, id: Int)
+
 }
